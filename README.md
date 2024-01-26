6d0cd6524ae481d4abc5d534132490fd9e03c7f0
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-01-26 10:37:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Crest - Your Non-Custodial Cross-Border Wallet
+
+Welcome to Crest – the ultimate solution for secure and low-cost cross-border payments.
+
+## Overview
+
+Crest is a non-custodial wallet designed to simplify cross-border transactions, offering security and affordability.
+
+## Features
+
+- **Non-Custodial:** Maintain control over your funds securely.
+
+- **Low-Cost:** Enjoy cost-effective cross-border payments.
+
+## Building Crest
+
+To build Crest, follow these steps:
+
+1. **Installation:** Clone the repository and install the necessary dependencies.
+
+   ```bash
+   git clone https://github.com/crest-project/crest.git
+   cd crest
+   yarn install
+   ```
+
+1. Configuration: Configure your settings and API keys in the ``.env``.
+
+   ```   
+   THIRDWEB_API_KEY=
+   COVALENT_API_KEY=
+   AWS_RECEIPIENT_EMAIL=
+   AWS_ACCESS_KEY_ID=
+   AWS_SECRET_ACCESS_KEY=
+   AWS_REGION=
+
+   ```
+
+2. Run Crest: Launch Crest on your local environment.
+
+```
+npm run dev
+```
+3. Explore the Documentation: Visit our documentation for in-depth information on using Crest.
+
+## Toolkits
+
+- **thirdweb:** The complete web3 development toolkit. [More info](https://thirdweb.com/)
+- **Covalent:** A single, unified API for fast, scalable historical blockchain data across 200+ chains. [More info](https://www.covalenthq.com/)
+- **AWS SES:** A cloud-based email service provider that can integrate into any application for high volume email automation. [More info](https://aws.amazon.com/ses/)
+
+## Contributing
+We welcome contributions from the community! If you'd like to contribute to Crest, please follow our contribution [guidelines](/CONTRIBUTING.md).
+
+## Support and Community
+- Issues: If you encounter any issues or bugs, please report them [here](https://github.com/Goshen-DAO/crest/issues).
+- Discord: Connect with the community on our [Discord channel](https://discord.gg/6XntnfTnUw).
+- Twitter: Follow us on [Twitter](https://twitter.com/GoshenDAO).
+- LinkedIn: Connect with us on [LinkedIn](https://www.linkedin.com/company/goshen-dao).
+
+## License
+Crest is licensed under the [Apache License](/LICENSE.md).
+
+## Disclaimer
+**Use at Your Own Risk**
+
+The smart contracts and code provided herein are offered "as is," without any guarantee, representation, or warranty, whether express or implied, regarding the safety or correctness of the user interface, smart contracts, or code. There is no assurance that they will function as intended, and users may encounter delays, failures, errors, omissions, or loss of transmitted information.
+
+Moreover, the utilization of these smart contracts and code must adhere to applicable laws. Nothing in this repository should be construed as investment advice or legal guidance for any specific facts or circumstances. It is not a substitute for competent counsel. For any questions or concerns, it is strongly recommended to seek advice from a reputable attorney in your jurisdiction.
+
+Crest disclaims all liability for the use of the foregoing, and users are advised to exercise caution and proceed at their own risk.
---
+++ pages/_app.tsx
@@ -0,0 +1,151 @@
+import type { AppProps } from "next/app";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  embeddedWallet,
+  trustWallet,
+  rainbowWallet,
+  safeWallet,
+} from "@thirdweb-dev/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import "/styles/global.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "base";
+
+const factoryAddress = process.env.CREST_SMARTWALLET_FACTORY_ADDRESS as string;
+
+const clientAPI= process.env.THIRDWEB_API_KEY as string;
+const smartWalletOptions = {
+  factoryAddress: factoryAddress,
+  gasless: true,
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+    activeChain={{// === Required information for connecting to the network === \\
+      chainId: 80085, // Chain ID of the network
+      // Array of RPC URLs to use
+      rpc: ["https://artio.rpc.berachain.com/"],
+
+      // === Information for adding the network to your wallet (how it will appear for first time users) === \\
+      // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+      nativeCurrency: {
+        decimals: 18,
+        name: "Berachain Artio",
+        symbol: "BERA",
+      },
+      shortName: "berachainartio", // Display value shown in the wallet UI
+      slug: "berachain", // Display value shown in the wallet UI
+      testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+      chain: "Berachain", // Name of the network
+      name: "Berachain Artio", // Name of the network
+    }}
+      clientId={clientAPI}
+      supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        safeWallet({
+          personalWallets: [
+            metamaskWallet(),
+            coinbaseWallet(),
+            walletConnect(),
+            localWallet(),
+            embeddedWallet({
+              recommended: true,
+              auth: {
+                options: [
+                  "email",
+                  "google",
+                  "apple",
+                  "facebook",
+                ],
+              },
+            }),
+            trustWallet(),
+            rainbowWallet(),
+          ],
+        }),
+        localWallet(),
+        embeddedWallet({
+          recommended: true,
+          auth: {
+            options: [
+              "email",
+              "google",
+              "apple",
+              "facebook",
+            ],
+          },
+        }),
+        trustWallet(),
+        rainbowWallet(),
+      ]}
+    >
+      <ChakraProvider>
+        <Head>
+          <title>
+            Crest - A Non-Custodial Wallet for Secure, Low-cost Cross-Border Payment.
+          </title>
+          <meta
+            name="description"
+            content="A Non-Custodial Wallet leveraging blockchain technology. Experience secure and cost-effective cross-border payments while maintaining control over your funds. Join us on the journey towards a seamless and empowered financial future!"
+          />
+          <meta
+            property="og:title"
+            content="Crest - A Non-Custodial Wallet for Secure, Low-cost Cross-Border Payment"
+          />
+          <meta
+            property="og:description"
+            content="A Non-Custodial Wallet leveraging blockchain technology. Experience secure and cost-effective cross-border payments while maintaining control over your funds. Join us on the journey towards a seamless and empowered financial future!"
+          />
+          <meta
+            property="og:image"
+            content="/metadata.png"
+          />
+          <meta
+            property="og:url"
+            content="https://goshendao.com"
+          />
+          <meta property="og:type" content="website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:title"
+            content="Crest - Your Secure Financial Wallet for Easy Transactions"
+          />
+          <meta
+            name="twitter:description"
+            content="Experience the security and convenience of Crest – your trusted financial companion for seamless fund storage, transfers, and receipts."
+          />
+          <meta
+            name="twitter:image"
+            content="/metadata.png"
+          />
+          <meta
+            name="twitter:url"
+            content="https://goshendao.com"
+          />
+        </Head>
+
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+        <SpeedInsights />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
